1ac3573df28356d50c52c8a02143bfaff0391049
iambillmccann <bill.mccann@gmail.com>
2020-04-14 17:32:50 -0400

    Added a theme provider to the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
+// React imports
 import React, { Component } from "react";
 
+// Material Design
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+
+// My stuff
+import appearance from "./services/appearance";
+
+const initialState = {
+  theme: appearance.defaultTheme
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
   render() {
+    const { theme } = this.state;
     const greeting = "Good morning, ";
     const me = "Bill";
 
     return (
-      <div>
-        {greeting}
-        {me}
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container maxWidth="md">
+          <div>
+            {greeting}
+            {me}
+          </div>
+        </Container>
+      </MuiThemeProvider>
     );
   }
 }
