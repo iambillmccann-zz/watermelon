74786342957cd2a800d2b97c48a04d0e089a3d3d
Bill McCann <bill.mccann@gmail.com>
2020-04-20 06:54:14 -0400

    Refactor router back into the App component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
 // React imports
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-
-// Material Design
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { MuiThemeProvider } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 
-// My stuff
-import appearance from "./services/appearance";
+// My imports
+import Dashboard from "./components/dashboard/Dashboard";
 import AppHeader from "./components/layout/AppHeader";
-import Cards from "./components/Cards";
-
-const initialState = {
-  theme: appearance.defaultTheme
-};
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = initialState;
-  }
-
   render() {
-    const { theme } = this.state;
-
     return (
       <BrowserRouter>
-        <MuiThemeProvider theme={theme}>
+        <MuiThemeProvider>
           <CssBaseline />
           <AppHeader />
           <Container maxWidth="xl">
             <Box m={3}>
-              <Cards />
+              <Switch>
+                <Route path="/" exact>
+                  <Dashboard />
+                </Route>
+              </Switch>
             </Box>
           </Container>
         </MuiThemeProvider>
---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,13 @@
+// React imports
+import React, { Component } from "react";
+
+// My imports
+import AppTabs from "../layout/AppTabs";
+
+class Dashboard extends Component {
+  render() {
+    return <AppTabs />;
+  }
+}
+
+export default Dashboard;
