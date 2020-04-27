f3e707dcbfb6864ca2466eec5c6c61e798c00a48
Bill McCann <bill.mccann@gmail.com>
2020-04-26 22:29:48 -0400

    Add styling for form, refactor App for hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // React imports
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { MuiThemeProvider } from "@material-ui/core/styles";
+import { MuiThemeProvider, useTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
@@ -10,27 +10,29 @@ import Container from "@material-ui/core/Container";
 import Dashboard from "./components/dashboard/Dashboard";
 import AppHeader from "./components/layout/AppHeader";
 import HomeDetail from "./components/houses/HomeDetail";
+import SignIn from "./components/auth/SignIn";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <MuiThemeProvider>
-          <CssBaseline />
-          <AppHeader />
-          <Container maxWidth="xl">
-            <Box m={3}>
-              <Switch>
-                <Route path="/" exact component={Dashboard} />
-                <Route path="/dashboard" component={Dashboard} />
-                <Route path="/homes/:id" component={HomeDetail} />
-              </Switch>
-            </Box>
-          </Container>
-        </MuiThemeProvider>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  const theme = useTheme();
+
+  return (
+    <BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppHeader />
+        <Container maxWidth="xl">
+          <Box m={3}>
+            <Switch>
+              <Route path="/" exact component={Dashboard} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/homes/:id" component={HomeDetail} />
+              <Route path="/signin" component={SignIn} />
+            </Switch>
+          </Box>
+        </Container>
+      </MuiThemeProvider>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/auth/signIn.js
+++ src/components/auth/signIn.js
@@ -3,57 +3,64 @@ import React from "react";
 
 // Material UI
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(3)
+    }
+  }
+}));
 
 const SignIn = () => {
+  const handleSubmit = e => console.log(e);
+  const handleChange = e => console.log(e);
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="sm">
-        <Typography
-          component="div"
-          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
-        >
-          <form onSubmit={}>
-            <h5>Sign In</h5>
-            <TextField
-                      autoComplete="email"
-                      disabled={performingAction}
-                      error={!!(errors && errors.emailAddress)}
-                      fullWidth
-                      helperText={
-                        errors && errors.emailAddress
-                          ? errors.emailAddress[0]
-                          : ""
-                      }
-                      label="E-mail address"
-                      placeholder="john@doe.com"
-                      required
-                      type="email"
-                      value={emailAddress}
-                      variant="outlined"
-//                      onChange={this.handleEmailAddressChange}
-                    />
-                    <TextField
-                      autoComplete="current-password"
-                      disabled={performingAction}
-                      error={!!(errors && errors.password)}
-                      fullWidth
-                      helperText={
-                        errors && errors.password ? errors.password[0] : ""
-                      }
-                      label="Password"
-                      placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
-                      required
-                      type="password"
-                      value={password}
-                      variant="outlined"
-//                      onChange={this.handlePasswordChange}
-                    />
-          </form>
-        </Typography>
+        <form onSubmit={handleSubmit} className={classes.root}>
+          <h2>Sign In</h2>
+          <TextField
+            autoComplete="email"
+            //disabled={performingAction}
+            //error={!!(errors && errors.emailAddress)}
+            fullWidth
+            //helperText={
+            //  errors && errors.emailAddress ? errors.emailAddress[0] : ""
+            //}
+            label="E-mail address"
+            placeholder="john@doe.com"
+            required
+            type="email"
+            //value={emailAddress}
+            //variant="outlined"
+            onChange={handleChange}
+          />
+          <TextField
+            autoComplete="current-password"
+            //disabled={performingAction}
+            //error={!!(errors && errors.password)}
+            fullWidth
+            //helperText={errors && errors.password ? errors.password[0] : ""}
+            label="Password"
+            placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
+            required
+            type="password"
+            //value={password}
+            //variant="outlined"
+            onChange={handleChange}
+          />
+          <Button variant="contained" color="primary" disableElevation>
+            Sign In
+          </Button>
+        </form>
       </Container>
     </React.Fragment>
   );
