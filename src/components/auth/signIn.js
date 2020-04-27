835923ab5f91c1b05469f15431fdbe4c8a1920d8
Bill McCann <bill.mccann@gmail.com>
2020-04-26 22:55:07 -0400

    Refactor the form

--- src/components/auth/signIn.js
+++ src/components/auth/signIn.js
@@ -1,8 +1,8 @@
 // React imports
-import React from "react";
+import React, { useState } from "react";
 
 // Material UI
-import CssBaseline from "@material-ui/core/CssBaseline";
+// import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
@@ -17,52 +17,57 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const SignIn = () => {
-  const handleSubmit = e => console.log(e);
-  const handleChange = e => console.log(e);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+  const handleChange = e => {
+    if (e.event.id === "userId") setEmail(e.event.value);
+    else setPassword(e.event.value);
+  };
   const classes = useStyles();
 
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <form onSubmit={handleSubmit} className={classes.root}>
-          <h2>Sign In</h2>
-          <TextField
-            autoComplete="email"
-            //disabled={performingAction}
-            //error={!!(errors && errors.emailAddress)}
-            fullWidth
-            //helperText={
-            //  errors && errors.emailAddress ? errors.emailAddress[0] : ""
-            //}
-            label="E-mail address"
-            placeholder="john@doe.com"
-            required
-            type="email"
-            //value={emailAddress}
-            //variant="outlined"
-            onChange={handleChange}
-          />
-          <TextField
-            autoComplete="current-password"
-            //disabled={performingAction}
-            //error={!!(errors && errors.password)}
-            fullWidth
-            //helperText={errors && errors.password ? errors.password[0] : ""}
-            label="Password"
-            placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
-            required
-            type="password"
-            //value={password}
-            //variant="outlined"
-            onChange={handleChange}
-          />
-          <Button variant="contained" color="primary" disableElevation>
-            Sign In
-          </Button>
-        </form>
-      </Container>
-    </React.Fragment>
+    <Container maxWidth="sm">
+      <form onSubmit={handleSubmit} className={classes.root}>
+        <h3>Sign In</h3>
+        <TextField
+          id="userId"
+          autoComplete="email"
+          //disabled={performingAction}
+          //error={!!(errors && errors.emailAddress)}
+          fullWidth
+          //helperText={
+          //  errors && errors.emailAddress ? errors.emailAddress[0] : ""
+          //}
+          label="E-mail address"
+          placeholder="john@doe.com"
+          required
+          type="email"
+          value={email}
+          //variant="outlined"
+          onChange={handleChange}
+        />
+        <TextField
+          autoComplete="current-password"
+          //disabled={performingAction}
+          //error={!!(errors && errors.password)}
+          fullWidth
+          //helperText={errors && errors.password ? errors.password[0] : ""}
+          label="Password"
+          placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
+          required
+          type="password"
+          value={password}
+          //variant="outlined"
+          onChange={handleChange}
+        />
+        <Button variant="contained" color="primary" disableElevation>
+          Sign In
+        </Button>
+      </form>
+    </Container>
   );
 };
 
