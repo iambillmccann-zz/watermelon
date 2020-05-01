0632d89bb4ae113dae7d13824a97c68a285150de
iambillmccann <bill.mccann@gmail.com>
2020-05-01 18:19:02 -0400

    Added the floating action  button

--- src/components/houses/homeList.js
+++ src/components/houses/homeList.js
@@ -1,14 +1,25 @@
+// React import
 import React from "react";
+import { Link } from "react-router-dom";
 
+// Material UI import
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 
+// My components
 import HomeSummary from "./HomeSummary";
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     flexWrap: "wrap"
+  },
+  fab: {
+    position: "absolute",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2)
   },
   paper: {
     padding: theme.spacing(2),
@@ -42,6 +53,15 @@ const HomeList = () => {
           <HomeSummary />
         </Grid>
       </Grid>
+      <Fab
+        color="primary"
+        aria-label="add"
+        className={classes.fab}
+        component={Link}
+        to="/homes/new"
+      >
+        <AddIcon />
+      </Fab>
     </div>
   );
 };
